ecbf5209b37ac7062165abe189474edda7364a1a
Camden Pettijohn <camdenpettijohn@gmail.com>
2026-08-04 01:00:42 -0700

    Added nojekyll

--- src/serve_update.ps1
+++ src/serve_update.ps1
@@ -4,6 +4,7 @@ try {
   Remove-Item -Recurse -Force docs -ErrorAction SilentlyContinue
   python3 src/python/compress.py
   npx @11ty/eleventy --serve
+  New-Item -ItemType File -Path "docs\.nojekyll" -Force | Out-Null
 } finally {
   Set-Location $original
 }
